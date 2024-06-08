af1ff6fbb06daa108c07c40ae73efdd53d25abb3
VimbaiShoniwa <169089437+VimbaiShoniwa@users.noreply.github.com>
2024-06-08 16:21:43 +0200

    Added basic JavaScript to generate poem

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Roses are red, violets are blue",
+    autoStart: true,
+    delay: 20,
+    cursor: "🐾",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
